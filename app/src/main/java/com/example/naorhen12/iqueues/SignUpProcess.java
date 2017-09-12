6902fba9f8703642ac9f140ebe54947fa14e3c3a
Naor123 <30801974+Naor123@users.noreply.github.com>
2017-09-12 23:28:06 +0300

    fully added a registration code which adds user with the details in to t he DB

--- app/src/main/java/com/example/naorhen12/iqueues/SignUpProcess.java
+++ app/src/main/java/com/example/naorhen12/iqueues/SignUpProcess.java
@@ -1,34 +1,94 @@
 package com.example.naorhen12.iqueues;
 
+import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.w3c.dom.Text;
 
 public class SignUpProcess extends AppCompatActivity {
 
-    private DatabaseReference Mdbref;
-    private TextView myTxt;
+    private DatabaseReference databaseReference;
+    private FirebaseAuth firebaseAuth;
+    private TextView email,pWord,phone,plate,hatNum,name,lastName;
+    private ProgressDialog progressDialog;
+    private Spinner companies;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_process);
-       // Mdbref = FirebaseDatabase.getInstance().getReference().child("Name");
-//        myTxt = (TextView) findViewById(R.id.myTxt);
-//        Mdbref.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(DataSnapshot dataSnapshot) {
-//                String name = dataSnapshot.getValue().toString();
-//               // myTxt.setText(name);
-//            }
-//
-//            @Override
-//            public void onCancelled(DatabaseError databaseError) {
-//
-//            }
-//        });
-
+        //initializing views
+        firebaseAuth = FirebaseAuth.getInstance();
+        email = (TextView) findViewById(R.id.email);
+        pWord = (TextView) findViewById(R.id.pWord);
+        name = (TextView) findViewById(R.id.name);
+        lastName = (TextView) findViewById(R.id.lastName);
+        phone = (TextView) findViewById(R.id.phoneNum);
+        plate = (TextView) findViewById(R.id.plates);
+        hatNum = (TextView) findViewById(R.id.hatNum);
+        progressDialog = new ProgressDialog(this);
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        companies = (Spinner) findViewById(R.id.taxiCompanies);
 
     }
+
+    public void signUser(View v)
+    {
+        final String uEmail = email.getText().toString().trim();
+        String uPword = pWord.getText().toString().trim();
+        if(TextUtils.isEmpty(uEmail)|| TextUtils.isEmpty(uPword))
+        {//if the email or password are empty, you can't be registered
+            Toast.makeText(this,"email or password fields are empty",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        progressDialog.setMessage("registering user please wait.");
+        progressDialog.show();
+        firebaseAuth.createUserWithEmailAndPassword(uEmail,uPword)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        //checks if the request has been successfully sent
+                        if(task.isSuccessful())
+                        {
+                            Toast.makeText(SignUpProcess.this,"registration successful",Toast.LENGTH_SHORT).show();
+                            progressDialog.dismiss();
+                            addUser();
+
+                        } else {
+                            Toast.makeText(SignUpProcess.this,"registration failed,please try again",Toast.LENGTH_SHORT).show();
+                            progressDialog.dismiss();
+                        }
+                    }
+                });
+
+    }
+   public void addUser(){
+       //gets current registered user object.
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+            String uId= firebaseUser.getUid();
+            String uName= name.getText().toString().trim();
+            String uLastName = lastName.getText().toString().trim();
+            String uPhone = phone.getText().toString().trim();
+            String uHat = hatNum.getText().toString().trim();
+            String uPlate= plate.getText().toString().trim();
+            String uCompany = companies.getSelectedItem().toString().trim();
+        //creates an object which contains all above strings.
+       UserDetails userInfo= new UserDetails(uName,uLastName,uPhone,uHat,uPlate,uCompany);
+       databaseReference.child(uId).setValue(userInfo);
+    }
+
 }
---
+++ app/src/main/java/com/example/naorhen12/iqueues/UserDetails.java
@@ -0,0 +1,24 @@
+package com.example.naorhen12.iqueues;
+
+
+public class UserDetails {
+
+    public String name;
+    public String lastName;
+    public String phone_number;
+    public String hat_number;
+    public String plate_number;
+    public String companyName;
+
+    public UserDetails(String name,String lName,String phone,String hatNum,String plate,String company)
+    {
+        this.name=name;
+        this.lastName=lName;
+        this.phone_number=phone;
+        this.hat_number=hatNum;
+        this.plate_number=plate;
+        this.companyName=company;
+    }
+
+}
+
