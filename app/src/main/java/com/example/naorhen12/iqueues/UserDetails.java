de51e78e691e54209aaec23cea9a8311ad169875
arie ben arie <baryeha@mail.com>
2018-02-19 14:00:28 +0200

    change UserDetails Class

--- app/src/main/java/com/example/naorhen12/iqueues/UserDetails.java
+++ app/src/main/java/com/example/naorhen12/iqueues/UserDetails.java
@@ -20,5 +20,52 @@ public class UserDetails {
         this.companyName=company;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public String getHat_number() {
+        return hat_number;
+    }
+
+    public void setHat_number(String hat_number) {
+        this.hat_number = hat_number;
+    }
+
+    public String getPlate_number() {
+        return plate_number;
+    }
+
+    public void setPlate_number(String plate_number) {
+        this.plate_number = plate_number;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
 }
 
