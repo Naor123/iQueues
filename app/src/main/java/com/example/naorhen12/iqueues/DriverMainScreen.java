0431957545e96dea1db405bd1aae5f47d7ce7c75
arie ben arie <baryeha@mail.com>
2018-02-19 16:21:46 +0200

    change UserDetails Class

--- app/src/main/java/com/example/naorhen12/iqueues/DriverMainScreen.java
+++ app/src/main/java/com/example/naorhen12/iqueues/DriverMainScreen.java
@@ -3,12 +3,34 @@ package com.example.naorhen12.iqueues;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class DriverMainScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_driver_main_screen);
+        setContentView(R.layout.activity_driver_date_picker);
+
+        ArrayList<String> time_per_day  = new ArrayList<>();
+        time_per_day.add("08:00 - 08:20");
+        time_per_day.add("08:00 - 08:20");
+        time_per_day.add("08:00 - 08:20");
+        time_per_day.add("08:00 - 08:20");
+        time_per_day.add("08:00 - 08:20");
+        time_per_day.add("08:00 - 08:20");
+        time_per_day.add("08:00 - 08:20");
+        time_per_day.add("08:00 - 08:20");
+        time_per_day.add("08:00 - 08:20");
+        time_per_day.add("08:00 - 08:20");
+        time_per_day.add("08:00 - 08:20");
+        time_per_day.add("08:00 - 08:20");
+        time_per_day.add("08:00 - 08:20");
+        time_per_day.add("08:00 - 08:20");
+        time_per_day.add("08:00 - 08:20");
+        time_per_day.add("08:00 - 08:20");
+        time_per_day.add("08:00 - 08:20");
+        time_per_day.add("08:00 - 08:20");
 
     }
 }
